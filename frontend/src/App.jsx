bacea80ec3c1bc281443a3465322fffbe4ce381a
EMAYAN <emayanramalingam@gmail.com>
2026-08-25 12:39:47 -0300

    chore: remove font previewer and revert to outfit font as default

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { WorkoutProvider } from './context/WorkoutContext';
 import AppContent from './AppContent';
-import FontPreviewer from './components/FontPreviewer';
 
 function App() {
   return (
     <WorkoutProvider>
       <AppContent />
-      <FontPreviewer />
     </WorkoutProvider>
   );
 }
